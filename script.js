e541a58c59c5280e6c736a677d472131f8912c70
Your Name <elgunmemmedli95@gmail.com>
2020-03-05 17:27:20 -0800

    Cudo

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("hid").style.display="none";
+function Show(){
+    document.getElementById("Send").style.display="none";
+    document.getElementById("Send").style.display="block";
+    document.getElementById("hid").style.display="inline";
+    document.getElementById("bar").style.display="none";
+}
+function Hide(){
+    document.getElementById("Send").style.display="none";
+    document.getElementById("bar").style.display="none";
+    document.getElementById("bar").style.display="inline";
+    document.getElementById("hid").style.display="none";
+}
